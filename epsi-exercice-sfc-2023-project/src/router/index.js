a5a81c56cd13f9766510a2c8474bde16e008c6a1
Sayli <hessabilyas@gmail.com>
2024-02-20 11:54:14 +0100

    routing track

--- epsi-exercice-sfc-2023-project/src/router/index.js
+++ epsi-exercice-sfc-2023-project/src/router/index.js
@@ -8,15 +8,15 @@ const router = createRouter({
     {
       path: '/',
       name: 'home',
-      component: Titre
+      component: HomeView
     },
     {
-      path: '/about',
-      name: 'about',
+      path: '/track',
+      name: 'track',
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
+      component: Titre
     }
   ]
 })
