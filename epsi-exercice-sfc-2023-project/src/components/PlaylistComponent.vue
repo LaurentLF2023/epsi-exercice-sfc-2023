43749c9a0b71f4c2d203c7cd47ab53ca07b3e3eb
vincvincvincvinc <152969599+vincvincvincvinc@users.noreply.github.com>
2024-02-20 10:09:40 +0100

    second commit -> playlist component template creation

--- epsi-exercice-sfc-2023-project/src/components/PlaylistComponent.vue
+++ epsi-exercice-sfc-2023-project/src/components/PlaylistComponent.vue
@@ -1,10 +1,23 @@
-<script setup>
+<template>
+  <div>
+    <form @submit.prevent="createPlaylist">
+      <input type="text" v-model="playlistName" placeholder="Enter playlist name" required>
+      <button type="submit">Create Playlist</button>
+    </form>
+  </div>
+</template>
 
+<script setup>
+export default {
+  data() {
+    return {
+      playlistName: '',
+    };
+  },
+}
 </script>
 
-<template>
 
-</template>
 
 <style scoped>
 
